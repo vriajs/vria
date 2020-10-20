057d08fb5e7f46e36192b81bf3dfd9ef6a2b8bcf
pbutcher <pbutcher93@gmail.com>
2020-10-20 17:27:56 +0100

    Updates

---
+++ precache-manifest.7078349a42006d1b89565e21adc8a93e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9163c01d0762bddec808e9371cd6d2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "279f2c735e1997c622f6",
+    "url": "./static/js/2.328e6276.chunk.js"
+  },
+  {
+    "revision": "838fbc01770b9481452631cfc94f5532",
+    "url": "./static/js/2.328e6276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f3ffbf50da2a5c9d5bf",
+    "url": "./static/js/main.6c3f80ad.chunk.js"
+  },
+  {
+    "revision": "31ebf704f9eabf6d10ec",
+    "url": "./static/js/runtime-main.58107abf.js"
+  },
+  {
+    "revision": "71b5f33a51082e9a16feac50bb062ce3",
+    "url": "./static/media/docs.71b5f33a.md"
+  },
+  {
+    "revision": "8ddb5242e7d37b99f74ac07d2fc0ce65",
+    "url": "./static/media/vria-logo.8ddb5242.png"
+  }
+]);
